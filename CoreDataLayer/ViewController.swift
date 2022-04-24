2adf1cd7368ebf0bae621fd14f3302ff87678170
Fabio Nisci <fabionisci@gmail.com>
2022-04-24 21:09:04 +0200

    Initial Commit

---
+++ CoreDataLayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataLayer
+//
+//  Created by Fabio Nisci on 24/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
